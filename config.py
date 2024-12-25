4c0cb6b4ab8c05784b696d7bd8c05bb66f5a6e3d
AF <gulabsinghrajput008@gmail.com>
2024-12-25 06:29:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()  # Load environment variables from a .env file
 API_ID = os.getenv('API_ID', '26494161')  
 API_HASH = os.getenv('API_HASH', '55da841f877d16a3a806169f3c5153d3')  
 BOT_TOKEN = os.getenv('BOT_TOKEN', '7793828619:AAHnY60vTcElNV_Trmd_DE6pKi7y_RKqRO8')  
-MONGODB_URL = os.getenv('MONGODB_URL', 'your_mongodb_url')  # MongoDB URL
+MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb+srv://lokendrasaini9galaxy:f0TVDwu5pVrHn5i6@cluster0.zseht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')  # MongoDB URL
 ADMIN_ID = os.getenv('ADMIN_ID', '7170452349')  # Add admin ID
 BOT_IMAGE_URL = os.getenv('BOT_IMAGE_URL', 'https://envs.sh/_Do.jpg')  # Bot's image URL
